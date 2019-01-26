1005d36c9f746246a02b569feedc21767d8abf4d
Eliot Lash <fadookie@gmail.com>
2019-01-26 11:40:44 -0800

    test for CSV loading path

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Data;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        var reader = new Reader();
+        reader.read();
     }
 
     // Update is called once per frame
