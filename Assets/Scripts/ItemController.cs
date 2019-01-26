438e75ec8fcb8b60e2c8d17dade09f2f0e8bb50e
Kyperbelt <kyperbelt@users.noreply.github.com>
2019-01-26 15:01:21 -0800

    inventory changes
    jank inventory

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemController : MonoBehaviour
+{
+
+  public Button button;
+  public Image Icon;
+  public Text Name,Weight;
+    // Start is called before the first frame update
+    void Start()
+    {
+    button.onClick.AddListener(OpenInfo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  void OpenInfo() {
+    Debug.Log("info is oppened for "+Name.text);
+  }
+}
