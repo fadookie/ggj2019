6b9cab899bc23dbe07f062bade9b6607599981b7
Eliot Lash <fadookie@gmail.com>
2019-01-27 15:55:14 -0800

    wire up sound fx

--- Assets/Scripts/SfxPlayer.cs
+++ Assets/Scripts/SfxPlayer.cs
@@ -1,22 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class SfxPlayer : MonoBehaviour
 {
+    public static SfxPlayer instance;
+
     public AudioClip ClickSound;
     public AudioClip DropSound;
     public AudioClip PickupSound;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private AudioSource player;
+
+    public enum Sound
     {
-        
+        Click,
+        Drop,
+        Pickup
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        // Singleton setup;
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        player = GetComponent<AudioSource>();
+    }
+
+    void Start() {
+        GameDataManager.instance.Player.Inventory.ObserveAdd().AsObservable()
+            .Subscribe(_ => {
+                PlaySound(Sound.Pickup);
+            });
+        GameDataManager.instance.Player.Inventory.ObserveRemove().AsObservable()
+            .Subscribe(_ => {
+                PlaySound(Sound.Drop);
+            });
+    }
+
+    public void PlaySound(Sound sound) {
+        switch (sound) {
+            case Sound.Click:
+                PlayClip(ClickSound);
+                break;
+            case Sound.Drop:
+                PlayClip(DropSound);
+                break;
+            case Sound.Pickup:
+                PlayClip(PickupSound);
+                break;
+        }
+    }
+
+    private void PlayClip(AudioClip clip) {
+        player.PlayOneShot(clip);
     }
 }
